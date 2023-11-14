f9d4b160275f19ebe9e699e957e2c1f1492069a8
Fitality <info@fitality.app>
2023-11-14 19:46:54 +0100

    v0.2.1 Add progressbar setup

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { usePodcastStore } from '../stores/podcast'
+import progessBar from './progessBar.vue';
 const podcastStore = usePodcastStore()
 const episode = podcastStore.episode
 
@@ -13,37 +14,44 @@ function play() {
 </script>
 <template>
     <div class="navbar" v-if="episode.title.length > 0">
-        <div class="info">
-            <img :src="episode.image" width="75" alt="" />
-            <span class="title">{{ episode.title }}</span>
-        </div>
-        <div class="controls">
-            <button @click="play">
-                <font-awesome-icon icon="fa-solid fa-pause" v-show="episode.isPlaying" />
-                <font-awesome-icon icon="fa-solid fa-play" v-show="!episode.isPlaying" />
-            </button>
+        <progessBar />
+        <div class="navbar-content">
+            <div class="info">
+                <img :src="episode.image" width="75" alt="" />
+                <span class="title">{{ episode.title }}</span>
+            </div>
+            <div class="controls">
+                <button @click="play">
+                    <font-awesome-icon icon="fa-solid fa-pause" v-show="episode.isPlaying" />
+                    <font-awesome-icon icon="fa-solid fa-play" v-show="!episode.isPlaying" />
+                </button>
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped>
 .navbar {
-    display: flex;
-    padding: 10px;
-    background-color: #f8f8f8;
-    border-block: 1px solid #e5e5e5;
-    gap: 20px;
-    justify-content: space-between;
-    box-shadow: 0px 0px 7px 1px rgba(0,0,0,0.1);
+    box-shadow: 0px 0px 7px 1px rgba(0, 0, 0, 0.1);
     position: sticky;
     bottom: 0;
     left: 0;
     right: 0;
 }
 
+.navbar-content {
+    display: flex;
+    padding: 10px;
+    background-color: #f8f8f8;
+    border-block: 1px solid #e5e5e5;
+    gap: 20px;
+    justify-content: space-between;
+}
+
 .controls {
     display: flex;
     align-items: center;
+
     & button {
         border: 1px solid #e5e5e5;
         border-radius: 50%;
---
+++ src/components/progessBar.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+    <div class="progress-bar">
+        <button></button>
+    </div>
+</template>
+
+<style scoped>
+.progress-bar {
+    width: 100%;
+    height: 10px;
+    background-color: red;
+}
+</style>
